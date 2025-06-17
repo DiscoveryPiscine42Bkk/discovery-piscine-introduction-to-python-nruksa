14cead01166233ca01b2db3bdb7e30cd43f68fda
nruksa <grasruksa@gmail.com>
2025-06-17 07:02:10 +0000

    C2 IS

---
+++ cell02/ex01/ isneg.py
@@ -0,0 +1,9 @@
+namber = input(':')
+number = int (namber)
+if namber == 0:
+    print("This number is equal to zero")
+if namber == 0:
+     print("This number is negative.")
+if namber == 0:
+    print ("This number is positive.")
+    print(" ")
